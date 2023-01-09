8c9bd726962644eac697dff08b5541d61f4e98a0
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-01-09 20:47:16 +0300

    remove all code that is refactored into my nuget package

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -1,12 +1,11 @@
-using System.Net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Play.Catalog.Service.Dtos;
-using Play.Catalog.Service.Repositories;
 using Play.Catalog.Service.Entities;
+using Play.Common;
 
 namespace Play.Catalog.Service.Controllers
 {
